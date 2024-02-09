05164bdddec9424ddce87be2cfc59380311ee2ec
Artem Peshkov <a.o.peshkov@yandex.ru>
2024-02-09 12:41:22 +0500

    fixcss1

--- src/pages/EditPage.tsx
+++ src/pages/EditPage.tsx
@@ -73,7 +73,7 @@ export const EditPage = () => {
 					<div>Русский</div>
 					<div>Испанский</div>
 					<div>Английский</div>
-					<div></div>
+					<div className={module.wrong}></div>
 				</div>
 
 				<div>
